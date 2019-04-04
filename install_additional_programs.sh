0aefcf74a1566869c3653be07e41b5c7e2e570d7
Petr Novak <petr@umbr.cas.cz>
2019-04-04 22:03:30 +0200

    bug fix in installation

--- install_additional_programs.sh
+++ install_additional_programs.sh
@@ -4,6 +4,7 @@ cd
 wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.8.zip
 unzip fastqc_v0.11.8.zip
 chmod 755 ~/FastQC/fastqc
+mkdir -p  ~/bin
 ln -s ~/FastQC/fastqc ~/bin/fastqc
 # when asked set symlink to ~/bin/
 apt install fastx-toolkit sickle
