c4ec640ec2cac2c4c96573f1bca1b147320dc57a
Petr Novak <petr@umbr.cas.cz>
2019-04-26 10:38:44 +0200

    assembly scriupt added

---
+++ assembly.sh
@@ -0,0 +1,6 @@
+#PBS -l select=1:ncpus=2:mem=2gb walltime=2:00:00
+# the line above species that we request one computer node with
+# 2 processors(cpu) and 2 GB of RAM. computation will take max 2 hrs
+
+module add trinity-2.6.5
+Trinity --max_memory 2G --seqType fq --left WT_01_R1.fastq  --right WT_01_R2.fastq --output output_directory_trinity --CPU 2
