98a3cb593e17ce0c5178ddf0b8d10bfc2063a803
Petr Novak <petr@umbr.cas.cz>
2020-05-26 14:20:05 +0200

    assembly updated

--- scripts/assembly_script2_pbs.sh
+++ scripts/assembly_script2_pbs.sh
@@ -4,6 +4,8 @@
 # 4 processors(cpu) and 4 GB of RAM. computation will take max 1 hrs
 cd /storage/praha1/home/$LOGNAME  # go to your home directory on tarkil
 module add velvet-1.2.09
+module add fastx-0.0.14
+
 mkdir ngs_assembly2
 cd ngs_assembly2
 # get paired end reads
